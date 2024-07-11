8f981e5b8fcc034273af7a027653d8899072325e
Nirmal Ubuntu 22 <nirmalraj476@gmail.com>
2024-07-10 23:01:32 -0500

    finish v1.1 with all animations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const navLinkEls = document.querySelectorAll('.highlight');
 const sectionEls = document.querySelectorAll('section'); 
 
-let currentSection = 'Projects'
+let currentSection = 'Projects';
 window.addEventListener('scroll', () => {
     sectionEls.forEach(sectionEl => {
         if (window.scrollY >= sectionEl.offsetTop) {
@@ -12,7 +12,22 @@ window.addEventListener('scroll', () => {
     navLinkEls.forEach(navLinkEl => {
         if (navLinkEl.href.includes(currentSection)) {
             document.querySelector('.active').classList.remove('active');
-            navLinkEl.classList.add('active')
+            navLinkEl.classList.add('active');
         }
     })
+})
+
+// const blob = document.getElementById('blob');
+// document.body.onpointermove = event => {
+//     const { clientX, clientY } = event;
+
+//     blob.style.left = `${clientX}px`;
+//     blob.style.top = `${clientY}px`;
+// }
+
+const cursor = document.getElementById('blob')
+
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + window.scrollY + 'px';
 })
